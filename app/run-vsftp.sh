a48445782a05d3a42b3ca44e436626c52dd95748
Dennis Riis <dr@driis.dk>
2023-02-04 20:46:46 +0100

    executable

--- app/run-vsftp.sh
+++ app/run-vsftp.sh
@@ -4,6 +4,7 @@ $FTP_PASSWORD" | adduser --gecos '' --no-create-home  $FTP_USER
 mkdir -p /home/$FTP_USER/process
 chown $FTP_USER:$FTP_USER /home/$FTP_USER
 
+chmod +x /app/upload.sh
 printenv >> /etc/environment
 service cron start
 
